7cedc442ec3a3a4a78de64a85b26033f451f0a71
TeRdIkZaLan <terdikzalan@turr.hu>
2023-06-08 11:34:32 +0200

    étrend folytatása

--- sablon.py
+++ sablon.py
@@ -1,3 +1,4 @@
+"""
 from tkinter import *
 #Névjegy ablak
 def nevjegy():
@@ -90,3 +91,26 @@ teglatest.add_command(label ='Felszín',command =felszin,underline =0)
 teglatest.add_command(label ='Térfogat',command =terfogat,underline =0)
 menu2.config(menu =teglatest)
 foablak.mainloop()
+"""
+import tkinter as tk
+
+master = tk.Tk()
+master.title('Test')
+master.geometry("100x100+700+350")
+
+b1 = tk.Button(text='1')
+b1.grid(row=0, column=0)
+
+b2 = tk.Button(text='2')
+b2.grid(row=0, column=1)
+
+b3 = tk.Button(text='3')
+b3.grid(row=1, column=0)
+
+b4 = tk.Button(text='4')
+b4.grid(row=1, column=1)
+
+b5 = tk.Button(text='5')
+b5.grid(padx=20)
+
+tk.mainloop()
