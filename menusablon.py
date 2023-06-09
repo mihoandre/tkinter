479f2910958d8b1ba6fa40ab1777c86b23f0bd5d
mihoandre <mihoandredominik@turr.hu>
2023-06-09 13:20:32 +0200

    Elkészült főoldal, szteroid oldal kódolása

--- menusablon.py
+++ menusablon.py
@@ -14,14 +14,25 @@ foablak.config(menu=menubar)
 
 txt1 = Label(foablak, text="Fitness")
 txt2 = Label(foablak, text="Üdvözöljük az oldalunkon, reméljük a segítségére lehetünk.")
+txt3 = Label(foablak, text="Tulajdonosok: Terdik Zalán, Somlyai Tamás, Mihó André")
+txt4 = Label(foablak, text="Elérhetőségek:")
+txt5 = Label(foablak, text="1. Telefonszám: +36309876543")
+txt6 = Label(foablak, text="2. E-mail: Fitness@gmail.com")
+txt7 = Label(foablak, text="3. Cím: Kis Feri utca 56 Mucsaröcsöge 9999")
+
 
 
 can1=Canvas(foablak, width=500, height=500,)
 photo=PhotoImage(file="fooldal.png")
 item=can1.create_image(250,250, image=photo)
-can1.grid(row=5, column=3, rowspan=5, padx=10, pady=10)
+can1.grid(row=2, column=0, pady=0)
  
-txt1.grid(row=0, column=3,)
-txt2.grid(row=1, column=3)
+txt1.grid(row=0, column=0,)
+txt2.grid(row=1, column=0,)
+txt3.grid(row=10, column=0, sticky=W)
+txt4.grid(row=11, column=0, sticky=W)
+txt5.grid(row=12, column=0, sticky=W)
+txt6.grid(row=13, column=0, sticky=W)
+txt7.grid(row=14, column=0, sticky=W)
 
 foablak.mainloop()
